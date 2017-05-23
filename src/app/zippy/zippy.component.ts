505694d16a9a6518424d7fb881991001c0a220f1
Mathias CADET <mathiascadet@MacBook-Pro-de-Mathias.local>
2017-05-23 07:23:46 +0200

    zippy module done

--- src/app/zippy/zippy.component.ts
+++ src/app/zippy/zippy.component.ts
@@ -1,14 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'zippy',
     templateUrl: './zippy.component.html',
     styles: [`
-        a:hover {
+        .zippy-title:hover {
             cursor: pointer;
         }
     `]
 })
 export class ZippyComponent{
+    @Input() title : string;
+    isExpanded : boolean = false;
 
+    toggle() {
+        this.isExpanded = !this.isExpanded;
+    }
+    
 }
